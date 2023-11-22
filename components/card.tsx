eed3323c69d6eccfd490d45038396ea741ecb36b
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-11-22 23:43:20 +0700

    style: fix style

--- components/card.tsx
+++ components/card.tsx
@@ -10,9 +10,9 @@ export default function Card(card: CardProps) {
     const image = require(`@/public/images/cards/${card.imageSrc}`);
 
     return (
-        <div className="lg:flex flex justify-between items-start w-1/4 p-2">
+        <div className="lg:flex flex justify-between items-start w-1/3 p-2">
                 <div className="bg-white font-bold rounded-xl p-2">
-                    <h2 className="text-blue-900">{card.imageKey}</h2>
+                    <h2 className="text-blue-900 text-2xl">{card.imageKey}</h2>
                     <Image src={image} className="rounded-3xl object-cover w-full h-full" alt={card.imageAlt}/>
                 </div>
         </div>
