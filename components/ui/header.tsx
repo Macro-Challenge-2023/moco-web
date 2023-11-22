08b4d2de444b5de6f68e356279d79c18d4959ad1
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-11-22 23:59:32 +0700

    style: fix style

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
                     href="/cards"
                     className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
                 >
-                  Flash Cards
+                  Flash Cards Demo
                 </Link>
               </li>
             </ul>
